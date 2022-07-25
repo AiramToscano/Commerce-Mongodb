89769c8038fa962f4ecc314a500a0a07632b1820
Airam Toscano <airamlobato@gmail.com>
2022-07-25 17:54:12 -0400

    Feat: req10

---
+++ challenges/desafio10.js
@@ -0,0 +1,8 @@
+db.produtos.find({
+    valoresNutricionais: { $elemMatch: { tipo: "proteínas", 
+    $and: [
+                { percentual: { $gte: 30 } },
+                { percentual: { $lte: 40 } },
+            ] } },
+
+}, { _id: false, nome: true });
